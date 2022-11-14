ccfd02fb205891438d1fed3a7f03b3e86d818b01
SSaratKS <sarat@s3kdevelopers.com>
2022-11-14 18:28:44 +0000

    included new components with authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
 import Demo from './components/demo';
-import React, { useState, Fragment } from 'react';
+import React, { useContext, useState, Fragment } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const ctx = useContext(AuthContext);
+
   return (
     <Fragment className="App">
+      {/* 
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main> 
+      */}
       <h2>Expense item!</h2>
       <Demo />
     </Fragment>
